9d644c5f6eb8aa6077060dc3d8946cc9f0ca4aef
André Panzetti <andre@panzetti.com.br>
2015-01-09 15:10:07 -0300

    Update and rename bitcoin-price.php to index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Bitcoin Display Price</title>
+</head>
+<?php
+require_once('config.php');
+?>
+<body bgcolor=white>
+<b>Bitcoin Price - Coindesk</b> <br />
+<?= $btcCOINDESK; ?> <br /><br />
+<b>Bitcoin Price - BTC-e</b> <br />
+<?= $btcBTCE; ?> <br />
+
+</body>
+</html>
