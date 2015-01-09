e855245b03b63059140472c000618a285e93bd40
André Panzetti <andre@panzetti.com.br>
2015-01-09 14:07:23 -0300

    Create bitcoin-price.php

---
+++ bitcoin-price.php
@@ -0,0 +1,25 @@
+<?php
+function getPrice($url) {
+$decode = file_get_contents($url);
+return json_decode($decode, true);
+}
+$btcUSD = getPrice('https://btc-e.com/api/2/btc_usd/ticker');
+$btcBTCE = $btcUSD["ticker"]["last"];
+
+$btcUSD = getPrice('https://api.coindesk.com/v1/bpi/currentprice.json');
+$btcCOINDESK = $btcUSD["bpi"]["USD"]["rate"];
+
+$btcBTCE = round($btcBTCE, 2);
+$btcCOINDESK = round($btcCOINDESK, 2);
+
+?>
+<!DOCTYPE HTML>
+<html>
+<body bgcolor=white>
+Bitcoin Price - Coindesk <br />
+<?= $btcCOINDESK; ?> <br />
+Bitcoin Price - BTC-e <br />
+<?= $btcBTCE; ?> <br />
+
+</body>
+</html>
