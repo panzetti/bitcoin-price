7e4fbad31ac0e1752b2e56315a8c874962e392a6
André Panzetti <andre@panzetti.com.br>
2015-01-09 19:53:03 -0200

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,25 @@
 <!DOCTYPE HTML>
 <html>
-<head>
-<title>Bitcoin Display Price</title>
-</head>
+	<head>
+		<title>Bitcoin Display Price</title>
+		 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+		 <script>
+			function autoRefresh_div() {
+				$("#result").load("index.php");// a function which will load data from other file after x seconds
+		}
+ 				setInterval('autoRefresh_div()', 5000); // refresh div after 5 secs
+         </script>
+	</head>
 <?php
 require_once('config.php');
 ?>
 <body bgcolor=white>
-<b>Bitcoin Price - Coindesk</b> <br />
-<?= $btcCOINDESK; ?> <br /><br />
-<b>Bitcoin Price - BTC-e</b> <br />
-<?= $btcBTCE; ?> <br />
+	<div id="result">
+		<b>Bitcoin Price - Coindesk</b> <br />
+		<?= $btcCOINDESK; ?><br /><br />
 
+		<b>Bitcoin Price - BTC-e</b> <br />
+		<?= $btcBTCE; ?><br /> <br />
+	</div>
 </body>
 </html>
