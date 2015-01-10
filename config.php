555780c6beb427a271dec40d88876f2f812fa7d4
André Panzetti <andre@panzetti.com.br>
2015-01-09 23:30:14 -0200

    Update config.php
    Trabalhando com:
    
    Bitcoin BTC-e
    Bitcoin Coindesk
    Bitcoin Average
    Bitstamp (working)

--- config.php
+++ config.php
@@ -8,7 +8,16 @@ $btcBTCE = $btcUSD["ticker"]["last"];
 $btcUSD = getPrice('https://api.coindesk.com/v1/bpi/currentprice.json');
 $btcCOINDESK = $btcUSD["bpi"]["USD"]["rate"];
 
+$btcUSD = getPrice('https://api.bitcoinaverage.com/ticker/global/all');
+$btcAVERAGE = $btcUSD["USD"]["last"];
+
+// $btcUSD = getPrice('https://www.bitstamp.net/api/ticker/');
+// $btcBITSTAMP = $btcUSD["high"]["last"];
+
 $btcBTCE = round($btcBTCE, 2);
 $btcCOINDESK = round($btcCOINDESK, 2);
+$btcAVERAGE = round($btcAVERAGE, 2);
+// $btcBITSTAMP = round($btcBITSTAMP, 2);
 
 ?>
+
