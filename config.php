c22e5a222da3b57dad399d842e0f92d51e7a70e2
André Panzetti <andre@panzetti.com.br>
2015-01-11 11:04:36 -0200

    Update config.php
    Adicionado BLOCKCHAIN USD e BRL

--- config.php
+++ config.php
@@ -11,12 +11,20 @@ $btcCOINDESK = $btcUSD["bpi"]["USD"]["rate"];
 $btcUSD = getPrice('https://api.bitcoinaverage.com/ticker/global/all');
 $btcAVERAGE = $btcUSD["USD"]["last"];
 
+$btcUSD = getPrice('https://blockchain.info/pt/ticker');
+$btcBLOCKCHAINUSD = $btcUSD["USD"]["last"];
+
+$btcUSD = getPrice('https://blockchain.info/pt/ticker');
+$btcBLOCKCHAINBRL = $btcUSD["BRL"]["last"];
+
 // $btcUSD = getPrice('https://www.bitstamp.net/api/ticker/');
 // $btcBITSTAMP = $btcUSD["high"]["last"];
 
 $btcBTCE = round($btcBTCE, 2);
 $btcCOINDESK = round($btcCOINDESK, 2);
 $btcAVERAGE = round($btcAVERAGE, 2);
+$btcBLOCKCHAINUSD = round($btcBLOCKCHAINUSD, 2);
+$btcBLOCKCHAINBRL = round($btcBLOCKCHAINBRL, 2);
 // $btcBITSTAMP = round($btcBITSTAMP, 2);
 
 ?>
