c0780b50a990a2463a875b0e91ceec33f2d9094b
André Panzetti <andre@panzetti.com.br>
2015-01-12 08:39:13 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,32 +5,13 @@
 		 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 		 <script>
 			function autoRefresh_div() {
-				$("#result").load("index.php");// a function which will load data from other file after x seconds
-											
-		}
- 				setInterval('autoRefresh_div()', 10000); // refresh div after 10 secs
-         </script>
+				$("body").load("bitprice.php");// a function which will load data from other file after x seconds
+			}
+ 			setInterval('autoRefresh_div()', 6000); // refresh div after 6 secs
+		</script>
 	</head>
-<?php
-require_once('config.php');
-?>
+	
 <body bgcolor=white>
-	<div id="result">
-		<b>Bitcoin Price - BTC-e</b> <br />
-		$ <?= $btcBTCE; ?><br /> <br />
-		
-		<b>Bitcoin Price - Coindesk</b> <br />
-		$ <?= $btcCOINDESK; ?><br /><br />
-
-		<b>Bitcoin Price - BitcoinAverage</b> <br />
-		$ <?= $btcAVERAGE; ?><br /> <br />
-		
-		<b>Bitcoin Price - BLOCKCHAIN</b> <br />
-		$ <?= $btcBLOCKCHAINUSD; ?> - R$ <?= $btcBLOCKCHAINBRL; ?><br /> <br />
-		
-		<b>Bitcoin Price - BITSTAMP</b> <br />
-		$ <?= $btcBITSTAMP; ?><br /> <br />
-		
-	</div>
+	<?php include_once("bitprice.php") ?>
 </body>
 </html>
